118925fa63d04986a378b79f5dffbde563571103
Diego Alarcón <diego@asimov.cl>
2019-02-13 20:26:46 -0300

    controles slider

---
+++ src/uc-components.js
@@ -0,0 +1,12 @@
+import { tns } from "../node_modules/tiny-slider/src/tiny-slider"
+
+var slider = tns({
+    container: '.home-carousel',
+    items: 1,
+    controls: false,
+    autoplay: true,
+    slideBy: 'page',
+    navPosition: 'bottom',
+    autoplayPosition: 'bottom',
+    navContainer: '.carousel-nav-list'
+});
