7afeb5f43f88fe2e257c4d063d93ae56f4909c10
Elias <elias.leyton@gmail.com>
2020-10-15 23:43:54 -0300

    add lib

--- src/js/webcomponents.js
+++ src/js/webcomponents.js
@@ -1,1 +1,5 @@
-import './webcomponents/test.js';
+// webcomponents.js (v1 spec polyfills)
+// https://github.com/webcomponents/polyfills/tree/master/packages/webcomponentsjs#how-to-use
+import "@webcomponents/webcomponentsjs/webcomponents-bundle.js";
+
+import './webcomponents/TopBar.js';
