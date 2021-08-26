748fb4817a07c556188f7cf6149aae68ce2ee21c
Elias Leyton <elias.leyton@gmail.com>
2021-08-26 15:57:14 -0400

    update webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,7 +5,4 @@ mix
   .js(['src/js/uc-kitdigital.js'], 'dist/js/precompile.js')
   .babel('dist/js/precompile.js', 'dist/js/uc-kitdigital.js')
   .sass('src/sass/uc-kitdigital.scss', 'dist/css')
-  
-  .js('src/js/agenda/index', 'dist/js/agenda.js')
-
   .ejs('examples/*', 'dist')
